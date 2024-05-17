1d662a4d396c551b27aedfb4c962fe0b3dc91e4e
Satanic-Spawn <dhruvjoshi892000@example.com>
2024-05-17 13:17:55 -0400

    Introduction of Gunicorn

--- API_Hands.py
+++ API_Hands.py
@@ -36,6 +36,9 @@ def process_frame():
     return jsonify({'sentence': sentence, 'prev_prediction': prev_prediction}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
+
+# gunicorn -b 0.0.0.0:6969 API_Hands:app
+
 
 
--- hands_package/Build_Model.py
+++ hands_package/Build_Model.py
@@ -113,7 +113,7 @@ class BuildModel:
 
         x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
-        model = RandomForestClassifier(max_depth=3, n_jobs=-1)
+        model = RandomForestClassifier(max_depth=24, n_jobs=-1)
         model.fit(x_train, y_train)
 
         y_predict = model.predict(x_test)
