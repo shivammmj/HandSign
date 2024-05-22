b9b0d4e4bb65dac7cec41dbf0c4e781f8e733f1f
Satanic-Spawn <dhruvjoshi892000@example.com>
2024-05-22 17:09:05 -0400

    forgot this

--- API_Hands.py
+++ API_Hands.py
@@ -38,8 +38,8 @@ def process_frame():
     
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', debug=True)
-    app.run(host='0.0.0.0',port = 6969, debug=True) # local testing
+    app.run(host='0.0.0.0', debug=True)
+    # app.run(host='0.0.0.0',port = 6969, debug=True) # local testing
 
 
 
